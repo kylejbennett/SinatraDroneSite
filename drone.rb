9294286de3479604e2501326cdca1bf8a1099b0e
Kyle Bennett <kyle.j.bennett5@gmail.com>
2015-09-04 11:35:12 -0400

    Created form in contact.erb and used Mandrill to make it functional for sending emails to us

--- drone.rb
+++ drone.rb
@@ -16,3 +16,18 @@ end
 get "/services" do
 	erb :services
 end
+
+post "/contact" do
+	puts "The parameters of the message are " + params.inspect
+	m = Mandrill::API.new
+	message = {  
+	:subject=> params["subject"],  
+	:from_name=> params["from"],  
+	:text=> params["message"],  
+	:to=>[{:email=> "kyle.bennett.development@gmail.com", :name=> params["Stratus Optics"]}],  
+	:html=> "",  
+	:from_email=> params["emailfrom"]
+	} 
+	m.messages.send message
+	erb :contact
+end
